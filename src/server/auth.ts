346b144fecfe20e7ef6c2767a35a5afe990af237
Kashyap Sojitra <kkashyapsojitra@gmail.com>
2024-12-13 13:36:41 +0530

    feat: email browser language support

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -8,14 +8,16 @@ import {
 import { type Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
-import EmailProvider, { SendVerificationRequestParams } from "next-auth/providers/email";
+import EmailProvider, {
+  SendVerificationRequestParams,
+} from "next-auth/providers/email";
 import { env } from "@/env";
 import { db } from "@/server/db";
 import { users } from "./db/schema/user";
 import { accounts } from "./db/schema/accounts";
 import { sessions } from "./db/schema/sessions";
 import { nextAuthVerificationToken } from "./db/schema/nextAuthVerificationToken";
-
+import { getLocale } from "next-intl/server";
 
 export const sendVerificationRequest = async ({
   identifier: email,
@@ -24,22 +26,44 @@ export const sendVerificationRequest = async ({
 }: SendVerificationRequestParams) => {
   const { server, from } = provider;
   // Send the email with your preferred library (e.g., Nodemailer)
+  const locale = await getLocale();
+  console.log("locale", locale);
+
+  const emailContent = {
+    en: {
+      subject: "Your sign-in link for InterviewPrep",
+      text: `Sign in to InterPrep by clicking the link: ${url}`,
+      html: `
+        <h1>Your sign-in link for Interview Prep</h1>
+        <p>Please click the link below to verify your email address:</p>
+        <a href="${url}">${url}</a>
+        <p>This link will expire in 24 hours.</p>
+      `,
+    },
+    ko: {
+      subject: `InterviewPrep에 대한 로그인 링크`,
+      text: `다음 링크를 클릭하여 InterPrep에 로그인하십시오. ${url}`,
+      html: `
+        <h1>인터뷰 준비를 위한 로그인 링크</h1>
+        <p>이메일 주소를 확인하려면 아래 링크를 클릭하세요:</p>
+        <a href="${url}">${url}</a>
+        <p>이 링크는 24시간 후에 만료됩니다.</p>
+      `,
+    },
+  };
+  const content = emailContent[locale === "en" ? "en" : "ko"];
+  console.log("content", content);
+
   const nodemailer = require("nodemailer");
   const transport = nodemailer.createTransport(server);
   await transport.sendMail({
     to: email,
     from,
-    subject: "Your sign-in link for MyApp",
-    text: `Sign in to MyApp by clicking the link: ${url}`,
-    html: `
-    <h1>Your sign-in link for InterviewPrep</h1>
-    <p>Please click the link below to verify your email address:</p>
-    <a href="${url}">${url}</a>
-    <p>This link will expire in 24 hours.</p>
-  `,
+    subject: content.subject,
+    text: content.text,
+    html: content.html,
   });
 };
-
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -58,7 +82,6 @@ declare module "next-auth" {
   interface User extends DefaultUser {
     ticketCount: number; // Add custom fields here
   }
-
 }
 
 /**
@@ -74,10 +97,10 @@ export const authOptions: NextAuthOptions = {
         user: {
           ...session.user,
           id: user?.id,
-          ticketCount: user.ticketCount
+          ticketCount: user.ticketCount,
         },
-      }
-    }
+      };
+    },
   },
   adapter: DrizzleAdapter(db, {
     usersTable: users,
@@ -86,23 +109,22 @@ export const authOptions: NextAuthOptions = {
     verificationTokensTable: nextAuthVerificationToken,
   }) as Adapter,
   providers: [
-
     EmailProvider({
       from: env.EMAIL_USER,
       server: {
-        host: 'smtp.gmail.com',
-        port: 587,         // TLS port
+        host: "smtp.gmail.com",
+        port: 587, // TLS port
         secure: false,
         auth: {
           user: env.EMAIL_USER,
           pass: env.EMAIL_PASSWORD,
         },
       },
-      sendVerificationRequest
+      sendVerificationRequest,
     }),
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
     /**
      * ...add more providers here.
@@ -114,9 +136,9 @@ export const authOptions: NextAuthOptions = {
      * @see https://next-auth.js.org/providers/github
      */
   ],
-  pages:{
-    verifyRequest:'/verify-email',
-    signIn:'/login'
+  pages: {
+    verifyRequest: "/verify-email",
+    signIn: "/login",
   },
 };
 
