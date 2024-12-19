9d3582c146638ff0298d68cd5cdaae32d7fbe246
vikash <billgoescoding@gmail.com>
2024-12-19 12:44:08 +0530

    add locale to channel io

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,17 @@ import { useEffect } from "react";
 import Home from "./module/Home";
 import * as ChannelService from '@channel.io/channel-web-sdk-loader';
 import { env } from "@/env";
+import { getLocale } from "next-intl/server";
+import { useLocale } from "next-intl";
 
 export default function Page() {
-
+  const locale = useLocale()
   useEffect(() => {
     if (process.env.NODE_ENV === 'production') {
       ChannelService.loadScript()
       ChannelService.boot({
         "pluginKey": env.NEXT_PUBLIC_CHANNEL_IO_PLUGIN_KEY,
+        language: locale
       });
     }
   }, [])
